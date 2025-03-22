3f5a31e19ce306aa1a1d6cf1ee1a13c7805bc846
sunny <dance@kakao.com>
2025-03-22 19:31:05 +0900

    eslint설정파일 변경

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+export default {
+  rules: {
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
+    indent: ['error', 2],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-trailing-spaces': 'error',
+  },
+};
